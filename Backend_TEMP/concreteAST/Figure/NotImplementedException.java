c909fbee632e8b1bc23e67d2eee70fcdcb48b7fe
Jacob Nash <jpnash@mail.bradley.edu>
2016-10-25 09:35:16 -0500

    New main package for backend

--- Backend_TEMP/concreteAST/Figure/NotImplementedException.java
+++
@@ -1,9 +0,0 @@
-package concreteAST.Figure;
-
-public class NotImplementedException extends Exception
-{
-    public NotImplementedException()
-    {
-        super("Method is not implemented");
-    }
-}
--- Backend_TEMP/equations/GroundedClause.java
+++
@@ -1,6 +0,0 @@
-package equations;
-
-public class GroundedClause
-{
-
-}
