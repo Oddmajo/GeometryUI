a8e244504e544f600c1e0b8047fedc6dc03d8cc9
SarcasticEmu <rbillingsly@mail.bradley.edu>
2016-11-01 01:44:20 -0500

    Fix to GroundedClause
    I was using an outdated GroundedClause, so here is the update.

--- src/backend/ast/GroundedClause.java
+++ src/backend/ast/GroundedClause.java
@@ -2,76 +2,79 @@
 iTutor – an intelligent tutor of mathematics
 Copyright (C) 2016-2017 C. Alvin and Bradley University CS Students (list of students)
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+This program is distributed : the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 package backend.ast;
 
 import java.util.ArrayList;
+import backend.utilities.list.Utilities;
+import backend.ast.ASTException;
+import backend.utilities.exception.*;
 
 /**
  * A First Order Logic clause that describes a property about a geometric drawing
  * @author Chris Alvin
  * @author Drew Whitmire
  */
-public abstract class GroundedClause
+public abstract class GroundedClause implements Cloneable
 {
     
     // A unique integer identifier (from the hypergraph)
     private int clauseId;
     @SuppressWarnings("unused")
-    private void SetID(int id) { clauseId = id; }
-    public int GetID() { return clauseId; }
+    private void setID(int id) { clauseId = id; }
+    public int getID() { return clauseId; }
 
     // Intrinsic as defined theoretically: characteristics of a figure that cannot be proven.
     private boolean intrinsic;
-    public boolean IsIntrinsic() { return intrinsic; }
-    public void MakeIntrinsic() { intrinsic = true; mayBeSourceNode = false; }
+    public boolean isIntrinsic() { return intrinsic; }
+    public void makeIntrinsic() { intrinsic = true; mayBeSourceNode = false; }
 
     // For problem generation, indicate if this node is given 
     private boolean given;
-    public boolean IsGiven() { return given; }
-    public void MakeGiven() { given = true; }
+    public boolean isGiven() { return given; }
+    public void makeGiven() { given = true; }
 
     // Denotes: A + A -> A
     private boolean purelyAlgebraic = false;
-    public boolean IsPurelyAlgebraic() { return purelyAlgebraic; }
-    public void MakePurelyAlgebraic() { purelyAlgebraic = true; }
+    public boolean isPurelyAlgebraic() { return purelyAlgebraic; }
+    public void makePurelyAlgebraic() { purelyAlgebraic = true; }
 
     // Contains all predecessors
     private ArrayList<Integer> generalPredecessors;
-    public ArrayList<Integer> GetGeneralPredecessors() { return generalPredecessors; }
+    public ArrayList<Integer> getGeneralPredecessors() { return generalPredecessors; }
     
     // Contains only Relation-based predecessors
     private ArrayList<Integer> relationPredecessors;
-    public ArrayList<Integer> GetRelationPredecessors() { return relationPredecessors; }
+    public ArrayList<Integer> getRelationPredecessors() { return relationPredecessors; }
 
-    public boolean HasRelationPredecessor(GroundedClause gc) { return relationPredecessors.contains(gc.clauseId); }
-    public boolean HasGeneralPredecessor(GroundedClause gc) { return generalPredecessors.contains(gc.clauseId) || relationPredecessors.contains(gc.clauseId); }
+    public boolean hasRelationPredecessor(GroundedClause gc) { return relationPredecessors.contains(gc.clauseId); }
+    public boolean hasGeneralPredecessor(GroundedClause gc) { return generalPredecessors.contains(gc.clauseId) || relationPredecessors.contains(gc.clauseId); }
 
     // Contains all figure fact predecessor / components (e.g. a triangle has 3 segments, 3 angles, and 3 points, etc) 
     private ArrayList<Integer> figureComponents;
-    public ArrayList<Integer> GetFigureComponents() { return figureComponents; }
-    public void AddComponent(int component) { backend.utilities.ast_helper.Utilities.AddUnique(figureComponents, component); }
-    public void AddComponentList(ArrayList<Integer> componentList) { backend.utilities.ast_helper.Utilities.AddUniqueList(figureComponents, componentList); }
-
+    public ArrayList<Integer> getFigureComponents() { return figureComponents; }
+    public void addComponent(int component) {Utilities.addUnique(figureComponents, component); }
+    public void addComponentList(ArrayList<Integer> componentList) {Utilities.AddUniqueList(figureComponents, componentList); }
+    
 
     // Can this node be strengthened to the given node?
-    public boolean CanBeStrengthenedTo(GroundedClause gc) { return false; }
+    public boolean canBeStrengthenedTo(GroundedClause gc) { return false; }
     // For problems: if a theorem or result is obvious and should never be a real source node for a problem
 
     private boolean mayBeSourceNode = true;
-    public void SetNotASourceNode() { mayBeSourceNode = false; }
-    public boolean IsAbleToBeASourceNode() { return mayBeSourceNode; }
+    public void setNotASourceNode() { mayBeSourceNode = false; }
+    public boolean isAbleToBeASourceNode() { return mayBeSourceNode; }
 
     private boolean mayBeGoalNode = true;
-    public void SetNotAGoalNode() { mayBeGoalNode = false; }
-    public boolean IsAbleToBeAGoalNode() { return !intrinsic && mayBeGoalNode; }
+    public void setNotAGoalNode() { mayBeGoalNode = false; }
+    public boolean isAbleToBeAGoalNode() { return !intrinsic && mayBeGoalNode; }
 
     private boolean isObviousDefinition = false;
-    public void SetClearDefinition() { isObviousDefinition = true; }
-    public boolean IsClearDefinition() { return isObviousDefinition; }
+    public void setClearDefinition() { isObviousDefinition = true; }
+    public boolean isClearDefinition() { return isObviousDefinition; }
     
     
     /**
@@ -88,53 +91,53 @@ public abstract class GroundedClause
     }
     */
 
-    public void AddRelationPredecessor(GroundedClause gc)
+    public void addRelationPredecessor(GroundedClause gc)
     {
         if (gc.clauseId == -1)
         {
-            //Debug.WriteLine("ERROR: id is -1: " + gc.ToString());
-            backend.utilities.exception.ExceptionHandler.throwException(new ASTException("ERROR: id is -1: " + gc.toPrettyString()));
+            //Debug.WriteLine("ERROR: id is -1: " + gc.toString());
+            ExceptionHandler.throwException(new ASTException("ERROR: id is -1: " + gc.toPrettyString()));
         }
-        backend.utilities.ast_helper.Utilities.AddUnique(relationPredecessors, gc.clauseId);
+        Utilities.addUnique(relationPredecessors, gc.clauseId);
     }
-    public void AddGeneralPredecessor(GroundedClause gc)
+    public void addGeneralPredecessor(GroundedClause gc)
     {
         if (gc.clauseId == -1)
         {
-            backend.utilities.exception.ExceptionHandler.throwException(new ASTException("ERROR: id is -1: " + gc.toPrettyString()));
+            ExceptionHandler.throwException(new ASTException("ERROR: id is -1: " + gc.toPrettyString()));
         }
-        backend.utilities.ast_helper.Utilities.AddUnique(generalPredecessors, gc.clauseId);
+        Utilities.addUnique(generalPredecessors, gc.clauseId);
     }
-    public void AddRelationPredecessors(ArrayList<Integer> preds)
+    public void addRelationPredecessors(ArrayList<Integer> preds)
     {
         for (int pred : preds)
         {
             if (pred == -1)
             {
-                backend.utilities.exception.ExceptionHandler.throwException(new ASTException("ERROR: id is -1: " + pred));
+                ExceptionHandler.throwException(new ASTException("ERROR: id is -1: " + pred));
             }
-            backend.utilities.ast_helper.Utilities.AddUnique(relationPredecessors, pred);
+            Utilities.addUnique(relationPredecessors, pred);
         }
     }
-    public void AddGeneralPredecessors(ArrayList<Integer> preds)
+    public void addGeneralPredecessors(ArrayList<Integer> preds)
     {
         for (int pred : preds)
         {
             if (pred == -1)
             {
-                backend.utilities.exception.ExceptionHandler.throwException(new ASTException("ERROR: id is -1: " + pred));
+                ExceptionHandler.throwException(new ASTException("ERROR: id is -1: " + pred));
             }
-            backend.utilities.ast_helper.Utilities.AddUnique(generalPredecessors, pred);
+            Utilities.addUnique(generalPredecessors, pred);
         }
     }
 
     private boolean axiomatic;
-    public boolean IsAxiomatic() { return axiomatic; }
-    public void MakeAxiomatic() { axiomatic = true; mayBeSourceNode = false; }
-    public boolean IsAlgebraic() { return false; } // By default we will say a node is geometric
-    public boolean IsGeometric() { return true; }  //  and not algebraic
-    public boolean IsReflexive() { return false; }
-    public boolean Strengthened() { return false; }
+    public boolean isAxiomatic() { return axiomatic; }
+    public void makeAxiomatic() { axiomatic = true; mayBeSourceNode = false; }
+    public boolean isAlgebraic() { return false; } // By default we will say a node is geometric
+    public boolean isGeometric() { return true; }  //  and not algebraic
+    public boolean isReflexive() { return false; }
+    public boolean strengthened() { return false; }
 
     public GroundedClause()
     {
@@ -149,42 +152,64 @@ public abstract class GroundedClause
 
     // The justification for when a node is deduced
     protected String justification;
-    public String GetJustification() { return justification; }
-    public void SetJustification(String j) { justification = j; }
+    public String getJustification() { return justification; }
+    public void setJustification(String j) { justification = j; }
 
     //
     // For equation simplification
     //
-    public int multiplier;
+    private int multiplier;
+    public int getMulitplier() { return multiplier; }
+    public void setMultiplier(int x) { multiplier = x; }
     
-    public ArrayList<GroundedClause> CollectTerms()
+    public ArrayList<GroundedClause> collectTerms()
     {
-        return new ArrayList<GroundedClause>(backend.utilities.ast_helper.Utilities.MakeList(this));
+        return new ArrayList<GroundedClause>(Utilities.makeList(this));
     }
     
     
-    public boolean Equals(Object obj)
+    public boolean equals(Object obj)
     {
-        if (obj != null && obj instanceof GroundedClause)
-        {
-            GroundedClause that = (GroundedClause) obj;
-            return multiplier == that.multiplier; // && clauseId == that.clauseId;
-        }
-        return false;
+        GroundedClause that = (GroundedClause) obj;
+        if (that == null) return false;
+        return multiplier == that.multiplier; // && clauseId == that.clauseId;
     }
 
-    public boolean StructurallyEquals(Object obj) { return false; }
+    public boolean structurallyEquals(Object obj) { return false; }
 
     //
     // For substitution and algebraic Simplifications
     //
-    public boolean ContainsClause(GroundedClause clause) { return false; }
-    public void Substitute(GroundedClause c1, GroundedClause c2) { }
-    public GroundedClause DeepCopy() throws CloneNotSupportedException { return (GroundedClause)this.clone(); }
+    public boolean containsClause(GroundedClause clause)
+    {
+        if (this == clause)
+            return true;
+        return false;
+     }
+    public void substitute(GroundedClause c1, GroundedClause c2)  {  }
+    public GroundedClause deepCopy()
+    { 
+            try
+            {
+                return  (GroundedClause) this.clone();
+            }
+            catch (CloneNotSupportedException e)
+            {
+                // TODO Auto-generated catch block
+                ExceptionHandler.throwException(e);
+            } 
+            return this;
+        
+    }
 
-    public int GetHashCode() { return super.hashCode(); }
+    public int getHashCode() { return super.hashCode(); }
 
-    public abstract String toString();
+    public String toString()
+    {
+        return this.getClass().toString();
+    }
+    
+    
 
     public String toPrettyString() { return Integer.toString(clauseId); }
 
