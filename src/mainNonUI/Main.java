e03cdb74b911748792f8dcc40439fbadce3ebbe7
Jacob Nash <jpnash@mail.bradley.edu>
2016-11-01 18:09:18 -0500

    mainNonUI
    Inclusion of a Main.java for the nonUI.  Currently only creates a
    TestManager and then calls run

---
+++ src/mainNonUI/Main.java
@@ -0,0 +1,16 @@
+package mainNonUI;
+
+import backend.utilities.test.TestManager;
+
+public class Main
+{
+
+    public static void main(String[] args) throws Exception
+    {
+        // TODO Auto-generated method stub
+        TestManager managerBob = new TestManager();
+        
+        managerBob.run();
+    }
+
+}
