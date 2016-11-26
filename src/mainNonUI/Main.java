7538d4231085524e772f388ec749c5f71b9d2111
Jacob Nash <jpnash@mail.bradley.edu>
2016-11-25 18:44:44 -0600

    Refactoring of Channels, Cleanup of Testing Code
    NOTE: If creating a custom diagram through the UI, only use the segment
    and point constructors.  Support for other constructors is a WIP.
    NOTE: I will also begin to create methods for default diagrams that will
    be common use for testing purposes.

--- src/mainNonUI/Main.java
+++ src/mainNonUI/Main.java
@@ -1,6 +1,7 @@
 package mainNonUI;
 
 import backend.utilities.test.TestManager;
+import channels.fromUI.Diagram;
 
 public class Main
 {
@@ -17,6 +18,15 @@ public class Main
         long timeEnd = System.nanoTime();
         
         System.out.println("Run time: " + (timeEnd - timeStart)/1000000000 + "seconds");
+    }
+    
+    public static void receiveDiagram(Diagram d)
+    {
+        //Do stuff with the Diagram
+        //Currently there is an 
+        //      Arraylist<Segment> of Segments
+        //      Arraylist<Point> of Points
+        System.out.println(d.toString());
     }
 
 }
